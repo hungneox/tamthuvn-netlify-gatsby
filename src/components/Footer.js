aa4786e642e3d94b72b5e166e1c48cda2ecee389
Hung Nguyen <hung.nguyen@digia.com>
2020-06-22 00:45:26 +0300

    Add contact info to bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,106 +1,78 @@
-import React from 'react'
-import { Link } from 'gatsby'
+import React from 'react';
+import { Link } from 'gatsby';
 
 import logo from '../img/tamthuvn-logo.svg';
-import facebook from '../img/social/facebook.svg'
-import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
-import vimeo from '../img/social/vimeo.svg'
+import facebook from '../img/social/facebook.svg';
+import instagram from '../img/social/instagram.svg';
+import twitter from '../img/social/twitter.svg';
+import vimeo from '../img/social/vimeo.svg';
 
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-light has-text-white-ter">
-        <div className="content has-text-centered">
-          <img
-            src={logo}
-            alt="TamThu"
-            style={{ width: '14em', height: '10em' }}
-          />
+      <footer className='footer has-background-light has-text-white-ter'>
+        <div className='content has-text-centered'>
+          <img src={logo} alt='TamThu' style={{ width: '14em', height: '10em' }} />
         </div>
-        <div className="content has-text-centered has-background-light has-text-white-ter">
-          <div className="container has-background-light has-text-white-ter">
-            <div style={{ maxWidth: '100vw' }} className="columns">
-              <div className="column is-4">
-                <section className="menu">
-                  <ul className="menu-list">
+        <div className='content has-text-centered has-background-light has-text-white-ter'>
+          <div className='container has-background-light has-text-white-ter'>
+            <div style={{ maxWidth: '100vw' }} className='columns'>
+              <div className='column is-4'>
+                <section className='menu'>
+                  <ul className='menu-list'>
                     <li>
-                      <Link to="/" className="navbar-item">
+                      <Link to='/' className='navbar-item'>
                         Trang chủ
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/about">
+                      <Link className='navbar-item' to='/about'>
                         Giới thiệu
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/products">
+                      <Link className='navbar-item' to='/products'>
                         Sản phẩm
                       </Link>
                     </li>
                     <li>
-                      <Link className="navbar-item" to="/contact/examples">
+                      <Link className='navbar-item' to='/contact/examples'>
                         Liên hệ
                       </Link>
                     </li>
-
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className='column is-4'>
                 <section>
-                  <ul className="menu-list">
-                    <li>
-                      <Link className="navbar-item" to="/blog">
-                        Latest Stories
-                      </Link>
-                    </li>
-                    <li>
-                      <Link className="navbar-item" to="/contact">
-                        Contact
-                      </Link>
-                    </li>
+                  <ul className='menu-list'>
+                    <li className='navbar-item'>39 Nguyễn Thị Diệu, P6, Q3, TP. HCM</li>
+                    <li className='navbar-item'>ĐT: 028 39326 240</li>
+                    <li className='navbar-item'>Fax: 028 39326 245</li>
+                    <li className='navbar-item'>lienlac@tamtuvn.com</li>
                   </ul>
                 </section>
               </div>
-              <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
-                  <img
-                    src={facebook}
-                    alt="Facebook"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+              <div className='column is-4 social'>
+                <a title='facebook' href='https://facebook.com'>
+                  <img src={facebook} alt='Facebook' style={{ width: '1em', height: '1em' }} />
                 </a>
-                <a title="twitter" href="https://twitter.com">
-                  <img
-                    className="fas fa-lg"
-                    src={twitter}
-                    alt="Twitter"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                <a title='twitter' href='https://twitter.com'>
+                  <img className='fas fa-lg' src={twitter} alt='Twitter' style={{ width: '1em', height: '1em' }} />
                 </a>
-                <a title="instagram" href="https://instagram.com">
-                  <img
-                    src={instagram}
-                    alt="Instagram"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                <a title='instagram' href='https://instagram.com'>
+                  <img src={instagram} alt='Instagram' style={{ width: '1em', height: '1em' }} />
                 </a>
-                <a title="vimeo" href="https://vimeo.com">
-                  <img
-                    src={vimeo}
-                    alt="Vimeo"
-                    style={{ width: '1em', height: '1em' }}
-                  />
+                <a title='vimeo' href='https://vimeo.com'>
+                  <img src={vimeo} alt='Vimeo' style={{ width: '1em', height: '1em' }} />
                 </a>
               </div>
             </div>
           </div>
         </div>
       </footer>
-    )
+    );
   }
-}
+};
 
-export default Footer
+export default Footer;
